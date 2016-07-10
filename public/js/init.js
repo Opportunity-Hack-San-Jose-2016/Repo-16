e59742c161d9b3dc260d9f5a2944e2510f68beb3
Vaishampayan <vaisham92@gmail.com>
2016-07-09 23:34:31 -0700

    Vaisham: Finished the process step update

--- public/js/init.js
+++ public/js/init.js
@@ -3,6 +3,6 @@ $(document).ready(function() {
 });
 
 $(document).ready(function() {
-	  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-	  $('.modal-trigger').leanModal();
-	});
+	// the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+	$('.modal-trigger').leanModal();
+});
